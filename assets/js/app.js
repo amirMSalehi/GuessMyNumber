9ce0829ff4abc0ffb0d2fba8f2323470e8a03b08
amirMohammad <amir.m.salehi.8582@gmail.com>
2024-10-05 22:05:13 +0330

    commit

---
+++ assets/js/app.js
@@ -0,0 +1,49 @@
+// 'use strict';
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+// document.querySelector('.guess').value;
+// document.querySelector('.message').textContent = 'Hello'
+
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 50;
+
+// document.querySelector('.check').addEventListener('click', () => {
+//     const guess = Number(document.querySelector('.guess').value)
+//     console.log(guess);
+
+//     if (!guess) {
+//         document.querySelector('.message').textContent = 'No Number'
+//     } else {
+//         document.querySelector('.message').textContent = 'True SEXY'
+
+//     }
+// })
+// document.querySelector('.score').textContent = '30';
+// document.querySelector('.number').textContent = '50';
+// function logInputValue() {
+//   console.log(document.querySelector('.guess').value);
+// }
+
+// let randomNumber = Math.trunc(Math.random() * 20) + 1;
+// document.querySelector('.score').textContent = randomNumber;
+
+// Math.random() * 10
+// console.log(Math.trunc(Math.random() * 10));
+// document.querySelector('.score').textContent = 50;
+// document.querySelector('.number').textContent = 90;
+// document.querySelector('.guess').value;
+
+// function inputValueLogger() {
+//   console.log(document.querySelector('.guess').value);
+// }
+// function nameLogger() {
+//   console.log('hello i am');
+// }
+
+// document.querySelector('.check').addEventListener('click', function () {
+//   console.log('hello am listener');
+// });
+
+// function restPage() {
+//     location.reload();
+// }
